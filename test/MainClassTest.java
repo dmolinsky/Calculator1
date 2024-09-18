0aece88ecd53a895e85a7f4f75874a3718340836
samyuktha215 <samyuktha.basam@gmail.com>
2024-09-18 09:17:54 +0200

    changed variables

--- test/MainClassTest.java
+++ test/MainClassTest.java
@@ -1,16 +1,13 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.lang.annotation.Target;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainClassTest {
     @Test
     @DisplayName("Testing addition method")
     public void testAddMethod() {
-        int a = 5;
-        int b = 6;
+        int a = 5,b=6;
         int c = a + b;
         assertEquals(11,c);
 
@@ -18,8 +15,7 @@ public class MainClassTest {
     @Test
     @DisplayName("Testing subtract method")
     public void testSubtractMethod() {
-        int a = 5;
-        int b = 6;
+        int a = 5, b = 6;
         int c = a - b;
         assertEquals(-1,c);
     }
