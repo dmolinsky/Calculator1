2f0bd4fee39c2e50742ce1aa4456ed0afc17861d
Max Ekberg <120407501+Slatebeard@users.noreply.github.com>
2024-09-20 11:32:37 +0200

    added the calculator logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Trying to update altMain and pulling it into IntelliJ :)!");
+        Scanner scanner = new Scanner(System.in);
+
+        int variable1, variable2, result;
+        int choice;
+
+        System.out.print("Enter variable 1: ");
+        variable1 = scanner.nextInt();
+
+        System.out.print("Enter variable 2: ");
+        variable2 = scanner.nextInt();
+
+        do {
+            System.out.println("Choose an operation:");
+            System.out.println("1. Add");
+            System.out.println("2. Subtract");
+            System.out.println("0. Stop");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    result = variable1 + variable2;
+                    System.out.println("Result: " + result);
+                    break;
+                case 2:
+                    result = variable1 - variable2;
+                    System.out.println("Result: " + result);
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Wrong choice, try again.");
+            }
+        } while (choice != 0);
+
     }
 }
