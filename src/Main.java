64d36a548095cf6effd6266b1d3b48877ff1f069
Diana Molinsky <diana.molinsky@iths.se>
2024-09-20 11:16:18 +0200

    Update Main.java to pull it into IntelliJ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test!");
+        System.out.println("Trying to update altMain and pulling it into IntelliJ :)!");
     }
 }
